74b335904063bded8ec880bd2b0ebd6beeb7dc47
sora0virus <definiteheavy@gmail.com>
2024-02-27 14:52:18 +0800

    new hello py

---
+++ hello.py
@@ -0,0 +1,11 @@
+print('hello world')
+print('what is your name?: ')
+
+myName=input()
+
+print('It is good to meet you, ' + myName)
+print('The length for your name is, ')
+print(len(myName))
+print('What is your age?') 
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' In a year.')
