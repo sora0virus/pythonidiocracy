8a4c093358d47d2240fb11bac2b405aa33daee19
sora0virus <definiteheavy@gmail.com>
2024-03-03 13:26:29 +0800

    new stuff

--- bob_age.py
+++ bob_age.py
@@ -3,7 +3,9 @@ age = 3000
 
 if name == 'Alice':
     print ('Hi Alice')
-elif age < 12:
+elif age < 12: #false
     print ('You are not ALICE.')
-elif age > 2000:
+elif age > 2000: #true
     print ('Unlike you, alice is not an immortal vampire.')
+elif age > 100: #skipped because the age > 200 is true and rest is skipped
+    print('You are not Alice, grannie.')
---
+++ enter_name.py
@@ -0,0 +1,6 @@
+print('Enter a name')
+name = input()
+if name != ' ':
+    print('Thank you for entering a name.') #when string
+else: 
+    print('You did not enter a name') #when u enter blank it triggers
